289c691658e7616a16f378b3602218632a2d2193
Michel Vorwieger <michel.vorwieger@outlook.de>
2020-10-29 02:30:15 +0100

    Added a basic Implementation of a Statistics class that you can save to a statisticsRepository that at this moment is just a mock implementation that stores the Statistics in memory

--- app.js
+++ app.js
@@ -51,7 +51,8 @@ class Human {
 }
 
 class Population {
-    constructor(minimumAgeOfReproduction, maximumAgeOfReproduction, avergageAgeOfDeath, amountChildren) {
+    constructor(id, minimumAgeOfReproduction, maximumAgeOfReproduction, avergageAgeOfDeath, amountChildren) {
+        this.id = id;
         this.minimumAgeOfReproduction = minimumAgeOfReproduction;
         this.maximumAgeOfReproduction = maximumAgeOfReproduction;
         this.avergageAgeOfDeath = avergageAgeOfDeath;
@@ -130,53 +131,69 @@ class Population {
 }
 
 
-function runSimulation({population, minimumAgeOfReproduction, maximumAgeOfReproduction, averageAgeOfDeath, childrenPerCouple}) {
-    const world = new Population(minimumAgeOfReproduction, maximumAgeOfReproduction, averageAgeOfDeath, childrenPerCouple);
+function runSimulation(id, {population, minimumAgeOfReproduction, maximumAgeOfReproduction, averageAgeOfDeath, childrenPerCouple}, afterOneYear) {
+    const world = new Population(id, minimumAgeOfReproduction, maximumAgeOfReproduction, averageAgeOfDeath, childrenPerCouple);
     world.addHumans(population);
-
 
     while (!world.isDistinct()) {
         world.passOneYear();
-        printSimulation(world);
+        afterOneYear(world);
     }
-
-    return world.yearsPassed;
 }
 
-/**
- * @param {Population} population
- */
-function printSimulation(population) {
-    const stats = {};
-    stats.year = population.yearsPassed
-    const men = population.population.filter(h => h.sex === "men");
-    const woman = population.population.filter(h => h.sex === "woman");
-    stats.population = {
-        men: men.length,
-        woman: woman.length,
-        total: population.population.length
-    };
-    stats.age = {
-        men: men.reduce((acc, h) => h.age + acc, 0) / men.length,
-        woman: woman.reduce((acc, h) => h.age + acc, 0) / woman.length,
-        total: population.population.reduce((acc, h) => h.age + acc, 0) / population.population.length
-    }
-    stats.reproduction = {
-        men: men.filter(h => h.canGetAChildren()).length,
-        woman: woman.filter(h => h.canGetAChildren()).length
-    }
+class Statistics {
+    year;
+    population;
+    age;
+    reproduction;
 
+    /**
+     * @param {Population} population
+     * @return {Statistics}
+     */
+    static fromPopulation(population) {
+        const stats = new Statistics();
+        stats.populationId = population.id;
+        stats.year = population.yearsPassed
+        const men = population.population.filter(h => h.sex === "men");
+        const woman = population.population.filter(h => h.sex === "woman");
+        stats.population = {
+            men: men.length,
+            woman: woman.length,
+            total: population.population.length
+        };
+        stats.age = {
+            men: men.reduce((acc, h) => h.age + acc, 0) / men.length,
+            woman: woman.reduce((acc, h) => h.age + acc, 0) / woman.length,
+            total: population.population.reduce((acc, h) => h.age + acc, 0) / population.population.length
+        }
+        stats.reproduction = {
+            men: men.filter(h => h.canGetAChildren()).length,
+            woman: woman.filter(h => h.canGetAChildren()).length
+        }
 
-    console.log(stats);
+        return stats;
+    }
 }
 
-const result = runSimulation({
+const statisticRepository = {
+    stats: new Map(),
+    save: function (s) {
+        if (!this.stats[s.populationId]) {
+            this.stats[s.populationId] = [];
+        }
+
+        this.stats[s.populationId].push(s);
+    }
+}
+
+runSimulation("myCoolSimmulation_v1", {
     population: 100,
     minimumAgeOfReproduction: 28,
     maximumAgeOfReproduction: 30,
     averageAgeOfDeath: 30,
     childrenPerCouple: () => Math.floor(Math.random() * 3)
-});
+}, world => statisticRepository.save(Statistics.fromPopulation(world)));
 
-console.log(result);
+console.dir(statisticRepository.stats, {depth: null});
 
